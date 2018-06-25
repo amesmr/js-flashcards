ecbca1556996d3f289a143d568fd94ea0e8c209d
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-25 14:15:35 -0400

    basic question schema skeleton

--- models/Question.js
+++ models/Question.js
@@ -2,9 +2,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const QuestionSchema = new Schema({
-
+    question: String,
+    choices: [String],
+    answer: Number,
+    tags: [String],
+    lesson: String,
+    goal: String
 });
 
 const Question = mongoose.model("Question", QuestionSchema);
 
 module.exports = Question;
+
