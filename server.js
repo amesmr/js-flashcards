8324ac4fe395828af4791a18b79190a9e762e295
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-25 16:24:11 -0400

    adding mongoose and morgan to server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-const express = require
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongod://localhost/checkPoint";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+
+
+
+
