44cbbfdccb9e56e6574103f82576abd6b2943454
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-25 17:00:19 -0400

    serverwork

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongod://localhost/checkPoint";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/checkPoint";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
