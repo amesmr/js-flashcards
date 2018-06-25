a90def32b85be45a7f387097705aba740ab029ac
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-25 14:12:22 -0400

    set up question schema skeleton

---
+++ models/Question.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const QuestionSchema = new Schema({
+
+});
+
+const Question = mongoose.model("Question", QuestionSchema);
+
+module.exports = Question;
