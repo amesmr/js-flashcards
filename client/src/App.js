1325651574507c6273ca15e231b2267bf7d1ea40
mfbradley <mbradleystylist@gmail.com>
2018-06-27 13:17:38 -0400

    updated App.js to include FlashCardContainer, removed default create-react-app elements

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import FlashCard from './components/FlashCard'
+import FlashCardContainer from './containers/FlashCardContainer';
 
 class App extends Component {
-
-  
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to Trilogy Study Buddy</h1>
+        <a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>Click to Start Studying!</a>
 
-        <FlashCard
-          question="This is a crazy question, holy shit"
-          answers ={["Answer A", "Answer B","Answer C","Answer D"]}
-          numberInSet={1}
-          answer="Answer B"
-          lesson="This is the lesson"
-          goal="This is the goal"
-          cpName="Checkpoint 5000"
-          hoverSwitch="on"
-          />
+        <FlashCardContainer />
       </div>
     );
   }
