f0889f783b7d67009bde0712c64a3e8c80546d05
mfbradley <mbradleystylist@gmail.com>
2018-06-27 10:51:00 -0400

    updated localhost to 9009 on storybook script; added App story to stories/index.js

--- client/src/stories/index.js
+++ client/src/stories/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withNotes } from '@storybook/addon-notes';
-import Button from '../components/Button';
+// import { action } from '@storybook/addon-actions';
+// import { withNotes } from '@storybook/addon-notes';
+import App from '../App';
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
-  ))
-  .add('with some emoji', () => (
-    withNotes('A very simple component')(() => <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>))
-  );   
 
+storiesOf("App", module)
+    .add("default", () => (
+        <App />
+    ));
   
   
   
