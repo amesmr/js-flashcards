99463cf60c44b5c66c63c98f19b849c198b4209a
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-27 11:34:13 -0400

    creating a flashcard that is more reusable

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -8,20 +8,26 @@ class FlashCard extends Component {
         super(props)
     
         this.state = {
-          flipped: false
+          flipped: false,
+          selected: '',
+          correct: false
         }
     
         this.checkAnswer = this.checkAnswer.bind(this)
+        this.onAnswerSelected = this.onAnswerSelected.bind(this)
       }
       
       componentDidMount(){
           if (this.props.hoverSwitch === "off") {
+              console.log("This is working")
             this.refs.flipCardContainer.removeAttribute("id","hoverSwitch")
           } 
       }
     
       checkAnswer(event) {
         event.preventDefault();
+
+        console.log(this.state.selected)        
         if(!this.state.flipped) {
             this.refs.flipCardContainer.classList.add("hover")
           this.setState({
@@ -37,33 +43,46 @@ class FlashCard extends Component {
         }
       }
 
+      onAnswerSelected(event) {
+          this.setState({
+              selected: event.currentTarget.value
+          })
+      }
+
 
 
     render() {
         return (
-        <div ref="flipCardContainer" className="flashCard-container">
+        <div ref="flipCardContainer" id="hoverSwitch" className="flashCard-container">
             <div className="flipper">
                 <div className="front">
                     <h6>{this.props.question}</h6>
                     <form>
                     {this.props.answers.map((answer, iterator) => {
                         return (
+                            this.props.hoverSwitch === "off" ?
                             <p key={iterator}>
                                 <label>
-                                    <input className="with-gap" name="group1" type="radio" value={answer}  />
+                                    <input className="with-gap" name="group1" type="radio" value={answer} checked={this.state.selected === answer} onChange={this.onAnswerSelected}  />
+                                    <span>{answer}</span>
+                                </label>
+                            </p> 
+                            :
+                            <p key={iterator}>
+                                <label>
                                     <span>{answer}</span>
                                 </label>
                             </p>
                         )
                     })}
                     
-                    <input type="submit" onClick={this.checkAnswer} /> 
+                {this.props.hoverSwitch === "off" && <input type="submit" onClick={this.checkAnswer} />}
                     
                     
                     </form>
                 </div>
                 <div className="back">
-                    <p>You were correct!</p>
+                    <p>{this.props.answer === this.state.selected ? "You were correct!" : "Sorry, that is incorrect!"}</p>
                     <h6>The answer was {this.props.answer}</h6>
 
                     <p><i>{this.props.lesson}</i></p>
