e59c24a8fd2a1eb7b66c8b0f23163f3666607206
mfbradley <mbradleystylist@gmail.com>
2018-06-27 12:38:49 -0400

    added FlashCard properties from App.js to FlashCard component

--- client/src/containers/FlashCardContainer/FlashCardContainer.js
+++ client/src/containers/FlashCardContainer/FlashCardContainer.js
@@ -6,7 +6,16 @@ class FlashCardContainer extends Component {
     render() {
         return (
             <div className = "fcContainer">
-                <FlashCard />
+                <FlashCard 
+                    question="This is a crazy question, holy shit"
+                    answers ={["Answer A", "Answer B","Answer C","Answer D"]}
+                    numberInSet={1}
+                    answer="Answer B"
+                    lesson="This is the lesson"
+                    goal="This is the goal"
+                    cpName="Checkpoint 5000"
+                    hoverSwitch="on"
+                />
             </div>
         )
     }
