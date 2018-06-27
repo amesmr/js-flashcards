661ea3535080cb0014766424217c0bc9a3d5a9a0
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-27 12:54:54 -0400

    set up app.js for menubar testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FlashCard from './components/FlashCard'
+import MenuBar from './components/MenuBar';
 
 class App extends Component {
 
@@ -10,15 +10,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <FlashCard
+        <MenuBar />
+        <div className="container">
+          <FlashCard
           question="This is a crazy question, holy shit"
           answers ={["Answer A", "Answer B","Answer C","Answer D"]}
           numberInSet={1}
@@ -26,8 +20,10 @@ class App extends Component {
           lesson="This is the lesson"
           goal="This is the goal"
           cpName="Checkpoint 5000"
-          hoverSwitch="on"
+          hoverSwitch="off"
           />
+        </div>
+        
       </div>
     );
   }
---
+++ client/src/components/MenuBar/MenuBar.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react';
+import './MenuBar.css'
+
+class MenuBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>WOW A MENU BAR</div>
+        )
+    }
+}
+
+export default MenuBar;
