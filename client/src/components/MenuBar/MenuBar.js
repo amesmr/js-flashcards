812cf9f6c588042123d77e04c57c21163d17830c
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-28 14:01:28 -0400

    grid added to navbar, all five portions built, dropdown work in progress

--- client/src/components/MenuBar/MenuBar.js
+++ client/src/components/MenuBar/MenuBar.js
@@ -1,33 +1,80 @@
 import React , { Component } from 'react';
 import './MenuBar.css'
+import { Dropdown, Button, NavItem } from 'react-materialize';
+
 
 class MenuBar extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-
+            tagdropdown: false,
+            tags: ["Item"]
         }
+
+        this.openDropdown = this.openDropdown.bind(this)
+    }
+
+    openDropdown(filter) {
+
+        if(filter === "tag") {
+            if(this.state.tagdropdown) {
+                this.refs.dropdownmenu.classList.remove('active')
+                this.setState({
+                    tagdropdown: false
+                })
+            } else {
+                this.refs.dropdownmenu.classList.add('active')
+                this.setState({
+                    tagdropdown: true
+                })
+            }
+        }
+        
+            
     }
 
 
     render() {
         return (
-            <nav className="row">
-                <div className="col s1 flow-text">
-                    <a class="waves-effect waves-light btn">
-                        <i class="material-icons right">
-                            cloud
-                        </i>
-                        Reset
-                    </a>
+            <div className="menubar">
+                <button className="menuitem">Shuffle</button>
+                <button className="menuitem">Reset</button>
+                <div className="menuitem">
+                    <p>
+                        <label>
+                            <input className="with-gap" name="cardtype" type="radio" checked />
+                            <span>FlashCards</span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            <input className="with-gap" name="cardtype" type="radio" />
+                            <span>QuizCards</span>
+                        </label>
+                    </p>
                 </div>
-                <div className="col s1 flow-text">Shuffle</div>
-                <div className="col s1 flow-text">FlashCards</div>
-                <div className="col s1 flow-text">QuizCards</div>
-                <div className="col s2 offset-s4 flow-text">Module Drodivdown</div>
-                <div className="col s2 flow-text">Tag Drodivdown</div>
-            </nav>
+                <div className="menuitem">
+                    <button className="dropbtn" onClick={() => this.openDropdown("tag")}>Drop me!</button>
+                    <form className="dropdown-items" ref="dropdownmenu">
+                        <p>
+                            <label>
+                                <input type="checkbox" />
+                                <span>Red</span>
+                            </label>
+                        </p>
+                    </form>
+
+                </div>
+                <div className="menuitem">
+                    <Dropdown trigger={<button className="dropbtn">Drop me!</button>}>
+                        <NavItem>Something</NavItem>
+                        <NavItem>Something</NavItem>
+                        <NavItem>Something</NavItem>
+                        <NavItem>Something</NavItem>
+                    </Dropdown>
+                </div>
+            </div>
         )
     }
 }
