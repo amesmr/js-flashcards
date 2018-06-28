769ff1f03125df179ee1e4977b1b025abfc79731
Daniel Doyle <dbdoyle182@gmail.com>
2018-06-28 18:36:24 -0400

    changes

--- client/src/containers/FlashCardContainer/FlashCardContainer.js
+++ client/src/containers/FlashCardContainer/FlashCardContainer.js
@@ -1,23 +1,45 @@
 import React, { Component } from 'react';
 import FlashCard from '../../components/FlashCard';
+import MenuBar from '../../components/MenuBar';
 import './FlashCardContainer.css';
 
 class FlashCardContainer extends Component {
-    render() {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+          hoverSwitch: "on"
+        }
+    
+        this.hoverSwitchChange = this.hoverSwitchChange.bind(this)
+      }
+     
+    
+      hoverSwitchChange(dataFromMenu) {
+        this.setState({
+          hoverSwitch: dataFromMenu
+        })
+      }
+    
+      render() {
         return (
-            <div className = "fcContainer">
-            <h1>NAV BAR GOES HERE</h1>
-                <FlashCard 
-                    question="This is a crazy question, holy shit"
-                    answers ={["Answer A", "Answer B","Answer C","Answer D"]}
-                    numberInSet={1}
-                    answer="Answer B"
-                    lesson="This is the lesson"
-                    goal="This is the goal"
-                    cpName="Checkpoint 5000"
-                    hoverSwitch="on"
-                />
+        <div className="fcContainer">
+            <MenuBar 
+              hoverGrab={this.hoverSwitchChange}
+            />
+            <div className="container">
+              <FlashCard
+              question="This is a crazy question, holy shit"
+              answers ={["Answer A", "Answer B","Answer C","Answer D"]}
+              numberInSet={1}
+              answer="Answer B"
+              lesson="This is the lesson"
+              goal="This is the goal"
+              cpName="Checkpoint 5000"
+              hoverSwitch={this.state.hoverSwitch}
+              />
             </div>
+        </div>
         )
     }
 }
