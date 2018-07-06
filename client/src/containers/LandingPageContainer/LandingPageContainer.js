4662f26b727e266f86134a766a31eb0f2dc89a02
mfbradley <mbradleystylist@gmail.com>
2018-07-06 11:17:34 -0400

    refactored App.js to render LandingPageContainer; added logic from App.js to LandingPageContainer.js

--- client/src/containers/LandingPageContainer/LandingPageContainer.js
+++ client/src/containers/LandingPageContainer/LandingPageContainer.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import FlashCardContainer from '../FlashCardContainer'
 
 class LandingPageContainer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            ready: false
+        }
+
+        this.readyOrNot = this.readyOrNot.bind(this)
+    }
+
+    readyOrNot() {
+        this.setState({
+            ready: true
+        })
+    }
+
     render() {
-        return(
-            <div></div>
-        )
+        return (
+            <div className="App">
+                {!this.state.ready ?
+                    <div>
+                        <h1>Welcome to Trilogy Study Buddy</h1>
+                        <a className="waves-effect waves-light btn-large" onClick={this.readyOrNot}>Click to Start Studying!</a>
+                    </div>
+                    :
+                    <FlashCardContainer />
+                }
+            </div>
+        );
     }
 }
-
 export default LandingPageContainer;
