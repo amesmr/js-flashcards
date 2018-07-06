f88e5c45dbdbdbb6943abf90546e0447f7ae435c
mfbradley <mbradleystylist@gmail.com>
2018-07-06 12:02:35 -0400

    updated LandingPageContainer.css first round

--- client/src/containers/LandingPageContainer/LandingPageContainer.js
+++ client/src/containers/LandingPageContainer/LandingPageContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './LandingPageContainer.css';
 import FlashCardContainer from '../FlashCardContainer'
 
 class LandingPageContainer extends Component {
@@ -22,9 +23,13 @@ class LandingPageContainer extends Component {
         return (
             <div className="App">
                 {!this.state.ready ?
-                    <div>
-                        <h1>Welcome to Trilogy Study Buddy</h1>
-                        <a className="waves-effect waves-light btn-large" onClick={this.readyOrNot}>Click to Start Studying!</a>
+                    <div className="container">
+                        <div class="row">
+                            <div className="col m12 landingPage z-depth-3">
+                                <h1>Welcome to Trilogy Study Buddy</h1>
+                                <a className="waves-effect waves-light btn-large studyBtn" onClick={this.readyOrNot}>Click to Start Studying!</a>
+                            </div>
+                        </div>
                     </div>
                     :
                     <FlashCardContainer />
