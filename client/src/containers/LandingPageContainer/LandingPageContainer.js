dc3afd483c893a9536bf739c597b1855d65b58cc
mfbradley <mbradleystylist@gmail.com>
2018-07-06 11:06:17 -0400

    basic code foundation LandingPageContainer.js and index.js

---
+++ client/src/containers/LandingPageContainer/LandingPageContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class LandingPageContainer extends Component {
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default LandingPageContainer;
