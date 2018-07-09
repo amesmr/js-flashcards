16bb3e5425fb70af5253858b22ba8f357b3ba3b0
Daniel Doyle <dbdoyle182@gmail.com>
2018-07-09 12:08:01 -0400

    basic form built

--- client/src/components/MenuForm/MenuForm.js
+++ client/src/components/MenuForm/MenuForm.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-
-
-import React, { Component } from 'react'
+import './MenuForm.css'
 
 export default class MenuBar extends Component {
 
@@ -16,7 +14,28 @@ export default class MenuBar extends Component {
     render() {
         return (
         <div>
-            
+            <form>
+                <div className="form-section">
+                    <p>
+                        <label>
+                            <input className="with-gap" name="content-type" type="radio" value=""/>
+                            <span className="checkboxStyle">FlashCards</span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            <input className="with-gap" name="content-type" type="radio" value=""/>
+                            <span className="checkboxStyle">Take Quiz</span>
+                        </label>
+                    </p>
+                </div>
+                <div className="form-section">
+
+                </div>
+                <div className="form-section">
+                </div>
+                <button>Start</button>
+            </form>
         </div>
         )
     }
