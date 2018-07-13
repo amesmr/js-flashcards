eb7dc8f77bd0c70af875fff5a9e4b3611005212a
mfbradley <mbradleystylist@gmail.com>
2018-07-13 09:45:51 -0400

    updated flashcard size, color, and layout

--- client/src/components/FlashCard/FlashCard.js
+++ client/src/components/FlashCard/FlashCard.js
@@ -10,60 +10,60 @@ class FlashCard extends Component {
         super(props)
 
         this.state = {
-          flipped: false,
-          selected: '',
-          correct: false
+            flipped: false,
+            selected: '',
+            correct: false
         }
 
         this.checkAnswer = this.checkAnswer.bind(this)
         this.onAnswerSelected = this.onAnswerSelected.bind(this)
-      }
+    }
 
-      // On component mount (following API response) checks for the status of the card type switch and presents the appropriate card type
-      componentDidMount(){
-          if (this.props.hoverSwitch === "off") {
-              //console.log("This is working")
-            this.refs.flipCardContainer.removeAttribute("id","hoverSwitch")
-          }
-      }
-      // Any time the card type is switch update the DOM to reflect the user selection. Allows for switch mid study session
-      componentDidUpdate(){
+    // On component mount (following API response) checks for the status of the card type switch and presents the appropriate card type
+    componentDidMount() {
         if (this.props.hoverSwitch === "off") {
             //console.log("This is working")
-          this.refs.flipCardContainer.removeAttribute("id","hoverSwitch")
+            this.refs.flipCardContainer.removeAttribute("id", "hoverSwitch")
+        }
+    }
+    // Any time the card type is switch update the DOM to reflect the user selection. Allows for switch mid study session
+    componentDidUpdate() {
+        if (this.props.hoverSwitch === "off") {
+            //console.log("This is working")
+            this.refs.flipCardContainer.removeAttribute("id", "hoverSwitch")
         } else {
-            this.refs.flipCardContainer.setAttribute("id","hoverSwitch")
+            this.refs.flipCardContainer.setAttribute("id", "hoverSwitch")
         }
 
 
-      }
+    }
 
-      // Checks the user selection against answer if the user has chosen to use the quiz cards
-      checkAnswer(event) {
+    // Checks the user selection against answer if the user has chosen to use the quiz cards
+    checkAnswer(event) {
         event.preventDefault();
 
         //console.log(this.state.selected)
-        if(!this.state.flipped) {
+        if (!this.state.flipped) {
             this.refs.flipCardContainer.classList.add("hover")
-          this.setState({
-            flipped: true
-          })
-          //console.log("Flip to back")
+            this.setState({
+                flipped: true
+            })
+            //console.log("Flip to back")
         } else {
             this.refs.flipCardContainer.classList.remove("hover")
-          this.setState({
-            flipped: false
-          })
-          //console.log("Flip to front")
+            this.setState({
+                flipped: false
+            })
+            //console.log("Flip to front")
         }
-      }
+    }
 
-      // Stores the user's choice on quiz card in the state
-      onAnswerSelected(event) {
-          this.setState({
-              selected: event.currentTarget.value
-          })
-      }
+    // Stores the user's choice on quiz card in the state
+    onAnswerSelected(event) {
+        this.setState({
+            selected: event.currentTarget.value
+        })
+    }
 
     transFormQuestion = question => {
         let newQuestion = question;
@@ -84,54 +84,55 @@ class FlashCard extends Component {
 
     render() {
         return (
-        <div ref="flipCardContainer" id="hoverSwitch" className="flashCard-container">
-            <div className="flipper">
-                <div className="front">
-                    {/* <h6 className="question">{this.props.question}</h6> */}
-                    <span>{this.props.number}</span>
-                    <code className="question">
-                        <div dangerouslySetInnerHTML={{ __html: this.transFormQuestion(this.props.question) }} />
-                    </code>
-                    <form>
-                        <ul className="answerColumn">
-                    {this.props.answers.map((answer, iterator) => {
-                        return (
-                            this.props.hoverSwitch === "off" ?
-                            <li key={iterator}>
-                                <label>
-                                    <input className="with-gap" name="group1" type="radio" value={answer} checked={this.state.selected === answer} onChange={this.onAnswerSelected}  />
-                                    <span className="answerChoice">{answer}</span>
-                                </label>
-                            </li>
-                            :
-                            <li key={iterator}>
-                                <label>
-                                    <span>{answer}</span>
-                                </label>
-                            </li>
-                        )
-                    })}
-                        </ul>
+            <div ref="flipCardContainer" id="hoverSwitch" className="flashCard-container">
+                <div className="flipper">
+                    <div className="front">
+                        {/* <h6 className="question">{this.props.question}</h6> */}
 
-                {this.props.hoverSwitch === "off" ? <input type="submit" onClick={this.checkAnswer} /> : <button onClick={this.checkAnswer}>"Flip"</button>}
+                        <code>
+                            <div className="question" dangerouslySetInnerHTML={{ __html: this.transFormQuestion(this.props.question) }} />
+                        </code>
+                        <form class='fcForm'>
+                            <ul className="answerColumn">
+                                {this.props.answers.map((answer, iterator) => {
+                                    return (
+                                        this.props.hoverSwitch === "off" ?
+                                            <li className='answerListItem' key={iterator}>
+                                                <label>
+                                                    <input className="with-gap" name="group1" type="radio" value={answer} checked={this.state.selected === answer} onChange={this.onAnswerSelected} />
+                                                    <span className="answerChoice">{answer}</span>
+                                                </label>
+                                            </li>
+                                            :
+                                            <li key={iterator}>
+                                                <label>
+                                                    <span>{answer}</span>
+                                                </label>
+                                            </li>
+                                    )
+                                })}
+                            </ul>
+
+                            {this.props.hoverSwitch === "off" ? <input type="submit" onClick={this.checkAnswer} /> : <button className='flipBtn' onClick={this.checkAnswer}>"Flip"</button>}
 
 
-                    </form>
+                        </form>
+                        <span className='fcNumber'>{this.props.number}</span>
+                    </div>
+                    <div className="back">
+                        <span>{this.props.number}</span>
+                        <p>{this.props.hoverSwitch === "off" && (this.props.answer === this.state.selected ? "You were correct!" : "Sorry, that is incorrect!"
+                        )}</p>
+                        <h6>The answer was: </h6>
+                        <code className="answerReveal">{this.props.answer}</code>
+                        <p className="objective"><i>Objective: {this.props.objective}</i></p>
+                        <p className="description"><i>Description: {this.props.description}</i></p>
+                        <p className="cpName">{this.props.cpName}</p>
+                        {this.props.hoverSwitch === "off" ? <input type="reset" onClick={this.checkAnswer} /> : <button onClick={this.checkAnswer}>"Flip"</button>}
+
+                    </div>
                 </div>
-                <div className="back">
-                <span>{this.props.number}</span>
-                  <p>{ this.props.hoverSwitch === "off" && (this.props.answer === this.state.selected ? "You were correct!" : "Sorry, that is incorrect!"
-                    )}</p>
-                  <h6>The answer was: </h6>
-                  <code className="answerReveal">{this.props.answer}</code>
-                  <p className="objective"><i>Objective: {this.props.objective}</i></p>
-                  <p className="description"><i>Description: {this.props.description}</i></p>
-                  <p className="cpName">{this.props.cpName}</p>
-                  {this.props.hoverSwitch === "off" ? <input type="reset" onClick={this.checkAnswer} /> : <button onClick={this.checkAnswer}>"Flip"</button>}
-
-                </div>
-            </div>
-        </div>)
+            </div>)
     }
 }
 
