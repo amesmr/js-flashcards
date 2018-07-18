153fed6cf4d0e5df5897a07e723a8588585b16d7
Daniel Doyle <dbdoyle182@gmail.com>
2018-07-18 10:29:44 -0400

    fixed the checkboxes on menuform

--- client/src/components/MenuForm/MenuForm.jsx
+++ client/src/components/MenuForm/MenuForm.jsx
@@ -37,13 +37,13 @@ export default class MenuBar extends Component {
                     <h6 className='landingStudyTitle'>Study Style</h6>
                     <p className="col m6 radioSlt">
                         <label>
-                            <input className="with-gap" name="content-type" type="radio" checked value="FlashCards" onChange={this.props.handleFormSelection}/>
+                            <input className="with-gap" name="content-type" type="radio" value="FlashCards" onChange={this.props.handleFormSelection}/>
                             <span className="checkboxStyle">FlashCards</span>
                         </label>
                     </p>
                     <p className="col m6 radioSlt">
                         <label>
-                            <input className="with-gap" name="content-type" type="radio" checked value="Quiz" onChange={this.props.handleFormSelection}/>
+                            <input className="with-gap" name="content-type" type="radio" value="Quiz" onChange={this.props.handleFormSelection}/>
                             <span className="checkboxStyle">Take Quiz</span>
                         </label>
                     </p>
