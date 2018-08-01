e876b2000f0130220722929851527f78efa92466
Daniel Doyle <dbdoyle182@gmail.com>
2018-08-01 11:50:36 -0400

    begun reducer file

---
+++ client/src/reducers/fcContainer/fcContainerReducer.js
@@ -0,0 +1,18 @@
+import { NEXT_FC_INDEX, PREV_FC_INDEX } from './fcContainerConstants';
+
+const initialState = {
+    index: 0
+}
+
+const fcContainerReducer = (state = initialState, action) {
+    switch (action.type) {
+        case NEXT_FC_INDEX:
+            return {};
+        case PREV_FC_INDEX:
+            return {};
+        default: 
+            return state;
+    }
+}
+
+export default fcContainerReducer;
